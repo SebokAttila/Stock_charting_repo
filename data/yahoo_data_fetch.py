ff0daa5d0c4bf4f03a8b0c971f47c288df0e924c
attila.sebok20 <attila.sebok20@stud.uni-obuda.hu>
2026-04-28 17:15:15 +0200

    Renamed data file and redone made path variable in yahoo_data_fetch.py

--- data/yahoo_data_fetch.py
+++ data/yahoo_data_fetch.py
@@ -3,23 +3,22 @@ import os.path
 import pandas as pd
 import yfinance as yf
 
+CVS_PATH = "C:/Users/Attila/PycharmProjects/Stock_charting_repo/raw_data/Yahoo_OHLCV_Hourly.csv"
 
 def get_yahoo_data(symbol, start, end, interval):
     try:
-        return get_historical_data_from_csv()
+        return get_historical_data_from_csv(CVS_PATH)
     except:
         return get_historical_data_from_api(symbol, start, end, interval)
-
 
 def get_historical_data_from_api(symbol, start, end, interval):
 
     ticker = yf.Ticker(symbol)
     historical_dataframe = ticker.history(start=start, end=end, interval=interval)
 
-    historical_dataframe.to_csv("C:/Users/Attila/PycharmProjects/Stock_charting_repo/raw_data/Yahoo_OHLCV_Hourly.csv")
+    historical_dataframe.to_csv(CVS_PATH)
     return historical_dataframe
 
-
-def get_historical_data_from_csv():
-    historical_dataframe = pd.read_csv( "C:/Users/Attila/PycharmProjects/Stock_charting_repo/raw_data/Yahoo_OHLCV_Hourly.csv")
+def get_historical_data_from_csv(path=CVS_PATH):
+    historical_dataframe = pd.read_csv(CVS_PATH)
     return historical_dataframe
